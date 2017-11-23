2d9a0520419714c0e33346309f4c8733837ca99a
Arithmetics <brock.m.tillotson@gmail.com>
2017-11-23 10:18:32 -0800

    added devise and posts

---
+++ app/controllers/static_pages_controller.rb
@@ -0,0 +1,12 @@
+class StaticPagesController < ApplicationController
+
+  def home
+    if user_signed_in?
+      redirect_to posts_path
+    else
+      redirect_to new_user_session_path
+    end
+  end
+
+
+end
